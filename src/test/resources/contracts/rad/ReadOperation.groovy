5a5c67494a74105a47f807f417e658ed7fd10de2
ssoneric <101182680+ssoneric@users.noreply.github.com>
2022-03-09 12:57:12 +0100

    Update ReadOperation.groovy

--- src/test/resources/contracts/rad/ReadOperation.groovy
+++ src/test/resources/contracts/rad/ReadOperation.groovy
@@ -12,7 +12,7 @@ Contract.make {
           '''
     )
     request {
-        urlPath(regex("/dmi/v1/ch/C21F969B5F03D33D43E04F8F136E7682/data/ds/ncmp-datastore:running")) {
+        urlPath(regex("/ncmp/v1/ch/C21F969B5F03D33D43E04F8F136E7682/data/ds/ncmp-datastore:running")) {
             queryParameters {
                 parameter "resourceIdentifier": "myNamespace:MyElement=L2022cloudMan"
                 parameter "async": "false"
